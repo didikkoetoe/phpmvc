846429117320d8fbad90ba60e28e8ba48c23c8de
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-29 09:22:32 +0700

    membuat file htaccess untuk folder public

--- app/core/App.php
+++ app/core/App.php
@@ -2,9 +2,18 @@
 
 class App
 {
-	function __construct()
+	public function __construct()
 	{
-		echo "ok";
+		$url = $this->parseUrl();
+		var_dump($url);
+	}
+
+	public function parseUrl()
+	{
+		if (isset($_GET['url'])) {
+			$url = $_GET["url"];
+			return $url;
+		}
 	}
 }
 
