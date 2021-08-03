1d2f8530cd683a538ef92b725d150448e9e46102
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-03 19:08:57 +0700

    Menghubungkan dengan db menggunakan PDO

--- app/models/Programmer_model.php
+++ app/models/Programmer_model.php
@@ -2,35 +2,24 @@
 
 class Programmer_model
 {
-    private $prg = [
-        [
-            "nama" => "Joko Basuki",
-            "alamat" => 'Bandung',
-            "jurusan" => 'Teknik Mesin',
-            "umur" => 30
-        ],
-        [
-            "nama" => "Budi Sakti Perkasa",
-            "alamat" => 'Solo',
-            "jurusan" => 'Teknik Nuklir',
-            "umur" => 27
-        ],
-        [
-            "nama" => "Dhimas anggara",
-            "alamat" => 'Boyolali',
-            "jurusan" => 'Teknik industri',
-            "umur" => 38
-        ],
-        [
-            "nama" => "Rahmat Setiadi",
-            "alamat" => 'Jogja',
-            "jurusan" => 'Hukum',
-            "umur" => 26
-        ]
-    ];
+    private $dbh; //database handler
+    private $stmt;
+
+    public function __construct()
+    {
+        // database source name
+        $dsn = 'mysql:host=localhost;dbname=phpmvc';
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
     public function getAllPrg()
     {
-        return $this->prg;
+        $this->stmt = $this->dbh->prepare("SELECT * FROM programmer");
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
