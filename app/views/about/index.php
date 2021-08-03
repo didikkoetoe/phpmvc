ff5df63185153701e757cab11f9fa9b336f243d7
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-03 14:49:56 +0700

    Menambahkan gambar

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>Tentang saya</h1>
-<p>Hallo, nama saya <?= $data["nama"]; ?>, saya bekerja sebagai <?= $data["pekerjaan"]; ?>, dan umur saya saat ini adalah <?= $data["umur"]; ?> tahun</p>
+<div class="container">
+    <h1>Tentang saya</h1>
+    <img src="<?= BASEURL; ?>/img/me.png" class="rounded-circle shadow-lg" alt="">
+    <p>Hallo, nama saya <?= $data["nama"]; ?>, saya bekerja sebagai <?= $data["pekerjaan"]; ?>, dan umur saya saat ini adalah <?= $data["umur"]; ?> tahun</p>
+</div>
