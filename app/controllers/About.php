9cc726e2c07b4367821b57549f30dd29b64c893f
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-03 14:03:01 +0700

    membuat view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -7,11 +7,17 @@ class About extends Controller
 		$data["nama"] = $nama;
 		$data["pekerjaan"] = $pekerjaan;
 		$data["umur"] = $umur;
-		$this->view('about/index');
+		$data["judul"] = "About me";
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 
 	public function page()
 	{
+		$data["judul"] = "data";
+		$this->view('templates/header', $data);
 		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
