963c133fe903d81bc41b6d068335996924254071
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-03 14:50:42 +0700

    Membuat navbar

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,6 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["judul"]; ?></title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.min.css">
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">MVC</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>/1">Home</a>
+                <a class="nav-link" href="<?= BASEURL; ?>/about">About Me</a>
+            </div>
+        </div>
+    </div>
+</nav>
 
 <body>
