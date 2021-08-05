db86bab4add889ac71388c163a97bf1097bd5e07
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-05 18:59:22 +0700

    Menambahkan method detail

--- app/controllers/Programmer.php
+++ app/controllers/Programmer.php
@@ -10,4 +10,13 @@ class Programmer extends Controller
         $this->view("programmer/index", $data);
         $this->view("templates/footer");
     }
+
+    public function detail($id)
+    {
+        $data["judul"] = "Detail Programmer";
+        $data["programmer"] = $this->model('Programmer_model')->getPrgById($id);
+        $this->view("templates/header", $data);
+        $this->view("programmer/detail", $data);
+        $this->view("templates/footer");
+    }
 }
