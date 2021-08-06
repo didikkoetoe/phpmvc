b92a769d0e1703f88b6452d6a0ca5c04ea073be3
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-06 19:29:32 +0700

    Membuat form dari modal bootstrap

--- app/views/programmer/index.php
+++ app/views/programmer/index.php
@@ -1,6 +1,9 @@
 <div class="container mt-2">
     <div class="row">
         <div class="col-8">
+
+            <button type="button" class="btn btn-primary mb-3 mt-3" data-bs-toggle="modal" data-bs-target="#formModal">Tambah Data Programmer</button>
+
             <h2>Daftar Programmer</h2>
 
             <ul class="list-group">
@@ -10,6 +13,49 @@
                     </li>
                 <?php endforeach; ?>
             </ul>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Programmer</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/programmer/tambah" method="POST">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama Programmer</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="mb-3">
+                        <label for="alamat" class="form-label">Alamat</label>
+                        <input type="text" class="form-control" id="alamat" name="alamat">
+                    </div>
+                    <div class="mb-3">
+                        <label for="jurusan" class="form-label">Jurusan</label>
+                        <select class="form-select" aria-label="Default select example" id="jurusan" name="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Informasi">Teknik Informasi</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Psikologi">Psikologi</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="umur" class="form-label">Umur</label>
+                        <input type="number" class="form-control" id="umur" name="umur">
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
