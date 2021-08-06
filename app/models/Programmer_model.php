ffae42e588fe14699077bbae8d2a15287bf4a429
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-08-06 19:29:06 +0700

    Membuat method tambahDataPrg

--- app/models/Programmer_model.php
+++ app/models/Programmer_model.php
@@ -22,4 +22,20 @@ class Programmer_model
         $this->db->bind('id', $id);
         return $this->db->single();
     }
+
+    public function tambahDataPrg($data)
+    {
+        $query = "INSERT INTO programmer VALUES
+        ('', :nama , :alamat , :jurusan , :umur)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data["nama"]);
+        $this->db->bind('alamat', $data["alamat"]);
+        $this->db->bind('jurusan', $data["jurusan"]);
+        $this->db->bind('umur', $data["umur"]);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
